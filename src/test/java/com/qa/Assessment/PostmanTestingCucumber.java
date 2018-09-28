493aa9b6d9ae9307755c1ca4c57dac16c61d91b8
Lucy Hamilton <lwhamilton03@outlook.com>
2018-09-28 08:26:22 +0100

    add testing cucumber

---
+++ src/test/java/com/qa/Assessment/PostmanTestingCucumber.java
@@ -0,0 +1,17 @@
+package com.qa.Assessment;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTestingCucumber {
+
+}
